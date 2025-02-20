7e50ae42d1afa487414534e398ad35ad96b0fb01
Mark-Vil <markvil655@gmail.com>
2025-02-20 23:28:06 +0800

    aboutme updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import Hero from './components/Hero'
+import About from './components/About'
 import './App.css'
 
 function App() {
+  const scrollToAbout = () => {
+    const aboutSection = document.getElementById('about');
+    aboutSection?.scrollIntoView({ behavior: 'smooth' });
+  };
 
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Header onAboutClick={scrollToAbout} />
         <Routes>
           <Route path="/" element={<Hero />} />
         </Routes>
+        <About />
       </div>
     </Router>
   )
